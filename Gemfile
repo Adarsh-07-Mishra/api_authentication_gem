509782b79e7bfa6ff1ef64586fd45a00a85eb319
tania paiva <taniadaniela1803@gmail.com>
2025-04-19 10:58:52 -0300

    add Rspec tests

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source "https://rubygems.org"
+
+gemspec
+
+group :development, :test do
+  gem "rspec"
+  gem "pry"
+  gem "byebug"
+end
+
+group :test do
+  gem "activerecord"
+  gem "rspec-rails"
+  gem "sqlite3"
+  gem "bcrypt"
+  gem "jwt"
+end
--- lib/api_authentication_gem.rb
+++ lib/api_authentication_gem.rb
@@ -1,36 +1,13 @@
-require "jwt"
-require "bcrypt"
+require "api_authentication_gem/version"
+require "api_authentication_gem/configuration"
+require "api_authentication_gem/auth"
 
 module ApiAuthenticationGem
-  class Auth
-    SECRET_KEY = "your$ecretK3y"
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
 
-    def self.signup(email:, password:, user_class:)
-      return { error: "Email is required" } unless email
-      return { error: "Password must be at least 5 characters long" } if password.nil? || password.length < 5
-
-      return { error: "Email already exists" } if user_class.find_by(email: email)
-
-      password_digest = BCrypt::Password.create(password)
-      user = user_class.create(email: email, password_digest: password_digest)
-
-      { message: "User created", user: user }
-    end
-
-    def self.login(email:, password:, user_class:)
-      user = user_class.find_by(email: email)
-
-      return { error: "Invalid email or password" } unless user
-      return { error: "Invalid email or password" } unless BCrypt::Password.new(user.password_digest) == password
-
-      token = JWT.encode({ user_id: user.id, exp: Time.now.to_i + 3600 }, SECRET_KEY, 'HS256')
-      { token: token }
-    end
-
-    def self.decode_token(token)
-      JWT.decode(token, SECRET_KEY, true, { algorithm: 'HS256' })[0]
-    rescue
-      nil
-    end
+  def self.configure
+    yield(configuration)
   end
 end
